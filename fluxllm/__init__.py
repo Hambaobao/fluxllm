6afdde5c3381978c364d5cbb11be0e74dae0d14d
cyberswe <cyberswe@cyberswe.com>
2025-06-02 05:36:17 +0800

    Fix issue 7

--- fluxllm/__init__.py
+++ fluxllm/__init__.py
@@ -1,4 +1,4 @@
 from fluxllm.client_utils.cache import FluxCache
-from fluxllm.clients.openai import FluxOpenAI
+from fluxllm.clients.openai import FluxOpenAIChat
 
-__all__ = ["FluxOpenAI", "FluxCache"]
+__all__ = ["FluxOpenAIChat", "FluxCache"]
